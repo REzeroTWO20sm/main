95d154d87e6ef56fb6550100188474381b92b615
Robert Nikonorov <robert2005n@gmail.com>
2023-12-13 05:53:55 +0300

    CLEAR.

--- main/main.cpp
+++ main/main.cpp
@@ -1,15 +1,1 @@
-﻿#include <iostream>
-#include <string>
-using namespace std;
-int main() {
-	string a;
-	string b;
-	cout << "Enter your name: ";
-	cin >> a;
-	cout << endl;
-	cout << "END last name stupid b**ch: ";
-	cin >> b;
-	cout << endl;
-	cout << "Hello, " << a<< " Dibilovich";
-	return 0;
-}
+﻿
