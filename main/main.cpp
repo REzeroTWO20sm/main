33463f289be4f665a041bf6fb812308d4f686cce
Robert Nikonorov <robert2005n@gmail.com>
2023-12-13 05:34:13 +0300

    FIRST commiT.

---
+++ main/main.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+	string a;
+	cout << "Enter your name: ";
+	cin >> a;
+	cout << endl;
+	cout << "Hello, " << a;
+	return 0;
+}
